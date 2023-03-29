eee83489e9bc0f11e250032979dfac10e3fbdc94
Krish Goel <krishgoel3@gmail.com>
2023-03-29 19:23:48 +0530

    Removed the separate /dashboard route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,19 +4,19 @@
 	import { page } from '$app/stores';
 </script>
 
-<main class="min-h-screen py-[80px] bg-slate-900">
+<main class="min-h-screen pt-[120px] pb-[40px] bg-slate-900">
 	<div class="w-10/12 mx-auto">
-		{#if $page.data.session}
-			<p class="text-slate-200 text-base mb-10">
-				Signed in as {$page.data.session.user?.name ?? $page.data.session.user?.email}.
-			</p>
-			<!-- <a href="/auth/signout" class="button" data-sveltekit-preload-data="off">Sign out</a> -->
-		{:else}
-		<p class="text-slate-200 text-base mb-10">
-			Not signed in
-		</p>
-			<a href="/auth/signin" data-sveltekit-preload-data="off">Sign in</a>
-		{/if}
+		<nav class="fixed top-8 w-10/12 whitespace-nowrap overflow-x-auto rounded-lg py-4 px-8 bg-slate-700 ring-1 ring-slate-500 shadow-xl flex justify-between items-center">
+			<h2 class="text-xl font-bold pr-4 text-slate-100">VKYD Labs</h2>
+			<div class="whitespace-nowrap overflow-x-auto">
+				{#if $page.data.session}
+					<a href="/auth/{$page.data.session ? 'signout' : 'signin'}" class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-5 bg-sky-600 text-white hover:bg-indigo-700 duration-300 cursor-pointer shadow-xl hover:shadow-none sm:py-2 sm:px-1.5 sm:text-xs">
+						{$page.data.session ? 'Log Out' : 'Log In'}
+						<span aria-hidden="true" class="sm:inline pl-2 font-extra bold">→</span>
+					</a>
+				{/if}
+			</div>
+		</nav>
 	</div>
 
 	<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,58 @@
-<div class="w-10/12 mx-auto min-h-[calc(100vh-160px)] flex">
-	<div class="grid grid-cols-2 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-1 gap-8">
-		<div class="m-auto">
-			<h1 class="font-extrabold text-slate-200 text-5xl mb-4">LCFR</h1>
-			<h2 class="font-bold text-slate-200 text-2xl mb-4">
-				The Internal Management Tool for our 3D-Creation Partners at VKYD Labs.
-			</h2>
-			<p class="text-slate-200 text-base mb-10">
-				We are pioneering brand on-boarding and creation of digital assets for the new generation of
-				businesses. Welcome to <span class="font-bold">VKYD Labs</span>, we call this <span class="italic">Phygital Commerce</span>.
-			</p>
+<script>
+	import { page } from '$app/stores';
+</script>
 
-			<a href="/auth/signin" class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-5 bg-sky-600 text-white hover:bg-indigo-900 duration-300 cursor-pointer shadow-xl hover:shadow-none">
-				Log In
-				<span aria-hidden="true" class="sm:inline pl-2 font-extra bold">→</span>
-			</a>
-			<a href="/admin" class="text-sm font-semibold text-white hover:text-slate-300 duration-300 ml-6">
-				Admin Access 
-				<span aria-hidden="true" class="sm:inline pl-2 font-extra bold">→</span>
-			</a>
-		</div>
-		<div class="m-auto">
-			<img class="rounded-md bg-white/5 ring-1 ring-white/10" src="images/dashboard-screenshot.png" alt="App screenshot">
-		</div>
+<div class="w-10/12 mx-auto min-h-[calc(100vh-160px)] flex">
+	{#if $page.data.session}
+	<div class="mt-8">
+		<h3 class="font-semibold text-slate-400 text-lg">
+			Welcome Back
+		</h3>
+		<h2 class="font-extrabold text-slate-200 text-3xl mb-4">
+			{$page.data.session.user?.name}
+		</h2>
+		<p class="font-semibold text-slate-200 text-base mb-12">
+			{$page.data.session.user?.email}
+		</p>
+		<p class="font-regular text-slate-200 text-base mb-8">
+			Session expiry: {$page.data.session?.expires}
+		</p>
 	</div>
+	{:else}
+		<div class="grid grid-cols-2 xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-1 gap-8">
+			<div class="m-auto">
+				<h1 class="font-extrabold text-slate-200 text-5xl mb-4">LCFR</h1>
+				<h2 class="font-bold text-slate-200 text-2xl mb-4">
+					The Internal Management Tool for our 3D-Creation Partners at VKYD Labs.
+				</h2>
+				<p class="text-slate-200 text-base mb-10">
+					We are pioneering brand on-boarding and creation of digital assets for the new generation
+					of businesses. Welcome to <span class="font-bold">VKYD Labs</span>, we call this
+					<span class="italic">Phygital Commerce</span>.
+				</p>
+
+				<a
+					href="/auth/{$page.data.session ? 'signout' : 'signin'}"
+					class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-5 bg-sky-600 text-white hover:bg-indigo-700 duration-300 cursor-pointer shadow-xl hover:shadow-none sm:py-2 sm:px-1.5 sm:text-xs"
+				>
+					{$page.data.session ? 'Log Out' : 'Log In'}
+					<span aria-hidden="true" class="sm:inline pl-2 font-extra bold">→</span>
+				</a>
+				<a
+					href="/admin"
+					class="text-sm font-semibold text-white hover:text-slate-300 duration-300 ml-6"
+				>
+					Admin Access
+					<span aria-hidden="true" class="sm:inline pl-2 font-extra bold">→</span>
+				</a>
+			</div>
+			<div class="m-auto">
+				<img
+					class="rounded-md bg-white/5 ring-1 ring-white/10"
+					src="images/dashboard-screenshot.png"
+					alt="App screenshot"
+				/>
+			</div>
+		</div>
+	{/if}
 </div>
