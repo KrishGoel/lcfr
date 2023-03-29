839fef0e24dae82ba55d79ed23a99d449a7a9202
Krish Goel <krishgoel3@gmail.com>
2023-03-29 19:26:24 +0530

    Responsive sizing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 			<h2 class="text-xl font-bold pr-4 text-slate-100">VKYD Labs</h2>
 			<div class="whitespace-nowrap overflow-x-auto">
 				{#if $page.data.session}
-					<a href="/auth/{$page.data.session ? 'signout' : 'signin'}" class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-5 bg-sky-600 text-white hover:bg-indigo-700 duration-300 cursor-pointer shadow-xl hover:shadow-none sm:py-2 sm:px-1.5 sm:text-xs">
+					<a href="/auth/{$page.data.session ? 'signout' : 'signin'}" class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-5 bg-sky-600 text-white hover:bg-indigo-700 duration-300 cursor-pointer shadow-xl hover:shadow-none sm:py-2 sm:px-3 sm:text-xs">
 						{$page.data.session ? 'Log Out' : 'Log In'}
 						<span aria-hidden="true" class="sm:inline pl-2 font-extra bold">→</span>
 					</a>
