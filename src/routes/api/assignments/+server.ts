18096460517eb355f438d166ceec4e22d1569310
Krish Goel <krishgoel3@gmail.com>
2023-04-03 09:43:52 +0530

    Fixed RollupError: _insert is not exported by src/routes/+page.server.ts, imported by src/routes/api/assignments/+server.ts

--- src/routes/api/assignments/+server.ts
+++ src/routes/api/assignments/+server.ts
@@ -1,28 +1,28 @@
-import { json } from '@sveltejs/kit';
-import { _insert } from '../../+page.server';
+// import { json } from '@sveltejs/kit';
+// import { _insert } from '../../+page.server';
 
-export async function POST({ request }) {
-	const requestBody = await request.json();
-	try {
-		_insert(requestBody);
-		console.log(
-			'_insert from +page.server.js returned the following\n',
-			await _insert(requestBody),
-			'\n'
-		);
-		return json({
-			status: 200,
-			body: {
-				request: JSON.stringify(requestBody)
-			}
-		});
-	} catch (error) {
-		console.log(error);
-		return {
-			status: 500,
-			body: {
-				message: 'Kuch toh locha hai hmmm'
-			}
-		};
-	}
-}
+// export async function POST({ request }) {
+// 	const requestBody = await request.json();
+// 	try {
+// 		_insert(requestBody);
+// 		console.log(
+// 			'_insert from +page.server.js returned the following\n',
+// 			await _insert(requestBody),
+// 			'\n'
+// 		);
+// 		return json({
+// 			status: 200,
+// 			body: {
+// 				request: JSON.stringify(requestBody)
+// 			}
+// 		});
+// 	} catch (error) {
+// 		console.log(error);
+// 		return {
+// 			status: 500,
+// 			body: {
+// 				message: 'Kuch toh locha hai hmmm'
+// 			}
+// 		};
+// 	}
+// }
